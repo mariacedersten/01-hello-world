dcb466a7ad18e5284e3145853f1f6a94dda4dfd5
mariacedersten <s2401554@edu.bc.fi>
2025-05-11 21:27:16 +0300

    Tehty

---
+++ hello-world/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Business College!");
+        }
+    }
+}
